f555d91b1b9a534c627a63783c0d298dd5e4a7ac
sungtarbucks <sungtarbucks00@gmail.com>
2025-08-02 19:48:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import analyze_report
 
 st.set_page_config(page_title="전세가드", layout="wide")
-st.title("전세가드 리포트 v0.1.1")
+st.title("🏡 전세가드 리포트 v0.1.1")
 
 uploaded_file = st.file_uploader("등기부 등본 이미지를 업로드하세요", type=["jpg", "jpeg", "png"])
 
